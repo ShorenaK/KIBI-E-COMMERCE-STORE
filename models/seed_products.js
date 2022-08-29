3717611c8cfdfb1138e13cc59c9d0f7669b6ce08
ShorenaK <shorenaanzhilov@gmail.com>
2022-08-29 16:53:48 -0400

    seed_products for Keboard

--- models/seed_products.js
+++ models/seed_products.js
@@ -3,4 +3,28 @@ module.exports =[{
     price: 00, 
     image: '',
     id: 0
-},{},{},{},{}]
+},{
+name: '', 
+price: 00, 
+image: '',
+id: 1},{
+name: '', 
+price: 00, 
+image: '',
+id: 2},{
+name: '', 
+price: 00, 
+image: '',
+id: 3},{
+name: '', 
+price: 00, 
+image: '',
+id: 4},{
+name: '', 
+price: 00, 
+image: '',
+id: 5
+}]
+
+
+
