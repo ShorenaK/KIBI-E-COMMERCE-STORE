a48baa1270c45e989d4a65640280c50b00173504
StevieC7 <stevenrcalhoun@outlook.com>
2022-08-29 12:57:01 -0500

    Add routes for index and show page

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 router.use(express.json());
 router.use(express.urlencoded({ extended:true}));
 const seedProduct = require('../models/seed_products');
 const Product = require('../models/Product')
 
+router.get('/', (req,res) => {
+  const context = Product.find({})
+  res.render('index.ejs', {keyboards: context})
+})
 
-// testing
-
-
-
-
-
-
-
+router.get('/:id', (req,res) => {
+    const context = Product.findById(req.params.id)
+    res.render('show.ejs', {keyboard: context})
+})
 
 module.exports = router;
