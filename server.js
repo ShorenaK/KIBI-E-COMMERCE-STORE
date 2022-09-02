9d9951b02d408016dfe0c1783e22f9a797e0bcf1
ShorenaK <shorenaanzhilov@gmail.com>
2022-09-02 11:14:21 -0400

    Port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4005;
+const PORT = process.env.PORT || 4005;
 require('./config/db.connection');
 const methodOverride = require('method-override');
 const productsController = require('./controllers/products_controller')
